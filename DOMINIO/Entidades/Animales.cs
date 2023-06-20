17ba23444ed1d0a2ecc2ac41ca3581bf780c814e
Alexis Paiva <lelviejox@gmail.com>
2023-06-20 16:16:03 -0300

    Agregando RepositorioCliente

--- DOMINIO/Entidades/Animales.cs
+++ DOMINIO/Entidades/Animales.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace DOMINIO.Entidades
 {
-    internal class Animales
+    public class Animales
     {
+
     }
 }
---
+++ DOMINIO/Repositorio/ClienteRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using DOMINIO.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOMINIO.Repositorio
+{
+    public interface ClienteRepositorio
+    {
+        public List<Cliente> obtenerTodos();
+        public void grabar(Cliente cliente);
+        public void EliminarCliente(Guid idCliente);
+    }
+}
