f1c1ceea2513d31906bc804830c821ed33220962
Alexis Paiva <lelviejox@gmail.com>
2023-06-20 17:22:54 -0300

    Capa Infraestructura, utilizando repos en memoria

--- APLICACION/CrearCliente.cs
+++ APLICACION/CrearCliente.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using APLICACION.DTO;
+using DOMINIO.Entidades;
+using DOMINIO.Repositorio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,18 @@ using System.Threading.Tasks;
 
 namespace APLICACION
 {
-    internal class CrearCliente
+   public class CrearCliente
     {
+        private ClienteRepositorio repositorio;
+
+        public CrearCliente(ClienteRepositorio repositorio)
+        {
+            this.repositorio = repositorio;
+        }
+
+        public void ejecutar(ClienteDTO usuario)
+        {
+            this.repositorio.grabar(new Cliente(usuario.Id(), usuario.Nombre(), usuario.Apellido(), usuario.Email()));
+        }
     }
 }
---
+++ INFRAESTRUCTURA/ClienteRepositorioEnMemoria.cs
@@ -0,0 +1,29 @@
+﻿using DOMINIO.Entidades;
+using DOMINIO.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INFRAESTRUCTURA
+{
+    public class ClienteRepositorioEnMemoria : ClienteRepositorio
+    {
+        public void EliminarCliente(Guid idCliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<Cliente> clientes = new List<Cliente>();
+        public void grabar(Cliente cliente)
+        {
+            this.clientes.Add(cliente);
+        }
+
+        public List<Cliente> obtenerTodos()
+        {
+            return this.clientes;
+        }
+    }
+}
